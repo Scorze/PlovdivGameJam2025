bfa2a11738ac20ac34ce281bb4ddb53846bd78e0
Georgi Semkov <georgesemkov@gmail.com>
2025-01-24 21:27:22 +0200

    In progress

---
+++ Assets/_Project/Scripts/ClientNetworkTransform.cs
@@ -0,0 +1,16 @@
+﻿using Unity.Netcode.Components;
+using UnityEngine;
+
+namespace Kart {
+    public enum AuthorityMode {
+        Server,
+        Client
+    }
+    
+    [DisallowMultipleComponent]
+    public class ClientNetworkTransform : NetworkTransform {
+        public AuthorityMode authorityMode = Kart.AuthorityMode.Client;
+
+        protected override bool OnIsServerAuthoritative() => authorityMode == Kart.AuthorityMode.Server;
+    }
+}
